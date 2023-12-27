9caa3fa2fac2561049cfee0c6e44fc35bd19cda7
sawadi <sawadi@esensesoftware.com>
2023-12-27 17:38:16 +0300

    s

--- bin/www.js
+++ bin/www.js
@@ -9,8 +9,8 @@ const databaseURL = process.env.DATABASE_URL;
 app.set('port', port);
 
 //  mongoose.connect(databaseURL, {useNewUrlParser: true  });
- mongoose.connect('mongodb://localhost:27017/monroo', {useNewUrlParser: true  });
- //mongoose.connect('mongodb://username:password@localhost:27017/monroo', {useNewUrlParser: true  });
+ //mongoose.connect('mongodb://localhost:27017/monroo', {useNewUrlParser: true  });
+ mongoose.connect('mongodb://salah:salah4488366d@localhost:27017/monroo', {useNewUrlParser: true  });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
