fddd33ff79a1e0a9368f794a32d2f1c064b96249
Salah <salahalawadi26@gmail.com>
2024-06-01 06:15:14 +0300

    fix

--- apis/user.js
+++ apis/user.js
@@ -409,7 +409,7 @@ router.post('/ListProviders', auth, async function (req, res) {
            return returnError(res , "User info not detected");
         }
         if(isAll){
-            Provider.find({dob:{$and:{ $exists: true, $ne: null, $not: { $eq: "" } }}}, function(err, items) {
+            Provider.find({dob:{ $exists: true, $ne: null, $not: { $eq: "" }}}, function(err, items) {
                 if(err){
                     return returnError(res , err);
                 }else{
