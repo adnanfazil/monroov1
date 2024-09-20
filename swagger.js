544ed5333f91a398266ba2544d24ffcde645ece0
DESKTOP-CF9SRM6\lenovo <danifazil99@gmail.com>
2024-09-20 17:46:55 +0500

    Initial commit

--- swagger.js
+++ swagger.js
@@ -1,35 +1,41 @@
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Monroo API',
-        version: '1.0.0',
-        description: 'API documentation for Monroo application',
+  openapi: "3.0.0",
+  info: {
+    title: "Monroo API",
+    version: "1.0.0",
+    description: "API documentation for Monroo application",
+  },
+  servers: [
+    {
+      url: "https://vconnect.agency",
+      description: "Production server",
     },
-    servers: [{
-        url: process.env.API_URL || 'http://localhost:3000',
-        description: 'Development server',
-    }],
-    components: {
-        securitySchemes: {
-            BearerAuth: {
-                type: 'http',
-                scheme: 'bearer',
-                bearerFormat: 'JWT',
-            },
-        },
+  ],
+  components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
     },
+  },
 };
 
 const options = {
-    swaggerDefinition,
-    apis: ['./apis/*.js', './swagger-schemas.js'],
+  swaggerDefinition,
+  apis: ["./apis/*.js", "./swagger-schemas.js"],
 };
 
 const specs = swaggerJsdoc(options);
 
 module.exports = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+  );
 };
