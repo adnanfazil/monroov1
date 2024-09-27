15abd42340b5f9a687fcfaeff9612c44ccb0566c
DESKTOP-CF9SRM6\lenovo <danifazil99@gmail.com>
2024-09-27 17:33:35 +0500

    Testing v2

--- models/users.model.js
+++ models/users.model.js
@@ -1,24 +1,22 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-
 const schema = new Schema({
-   id: {type: String , unique: true},
-   name: {type: String , default: ""},
-   profilePic: {type: String , default: ""},
-   country: {type: String, default: ""}, // in our case we will always send UAE unless they will go with other country
-   isActive: {type: Boolean, default: false}, // after verify the email, will be true
-   username: {type: String, default: ""},
-   password: {type: String, default: ""},
-   registerDate: {type: String, default: ""},
-   phone: {type: String, default: ""},
-   email: {type: String, default: ""},
-   about: {type: String, default: ""},
-   companyName: {type: String, default: ""},
-   intrestedList: {type: [String], default: ""}, // list of  categories ids // set after register to add list of intrest
-   token: {type: String, default: ""},
-   fcmToken: {type: String, default: ""}
+  id: { type: String, unique: true },
+  name: { type: String, default: "" },
+  profilePic: { type: String, default: "" },
+  country: { type: String, default: "" }, // in our case we will always send UAE unless they will go with other country
+  isActive: { type: Boolean, default: false }, // after verify the email, will be true
+  username: { type: String, default: "" },
+  password: { type: String, default: "" },
+  registerDate: { type: String, default: "" },
+  phone: { type: String, default: "" },
+  email: { type: String, default: "" },
+  about: { type: String, default: "" },
+  companyName: { type: String, default: "" },
+  intrestedList: { type: [String], default: "" },
+  token: { type: String, default: "" },
+  fcmToken: { type: String, default: "" },
 });
 
-
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model("User", schema);
